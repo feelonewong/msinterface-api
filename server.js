1c6d982939fb5f91e992b384558d284d50d24e76
feelonewong <feelonewong@gmail.com>
2021-03-26 16:46:52 +0800

    链接mongodb数据库

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = require("./routes/mscamps");
 const logger = require("./middleware/logger");
 const morgan = require("morgan");
 const colors = require("colors");
-
+const connectDB = require("./config/db");
 dotenv.config({
   path: "./config/config.env",
 });
@@ -22,10 +22,21 @@ app.get("/", (req, res) => {
     })
 });
 
+//链接数据库
+connectDB();
 
 app.use(`/api/v1/mscamps`, router);
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server runnint on ${process.env.NODE_ENV} mode on port ${PORT}`.green)
 );
+
+
+process.on("unhandledRejection", (err, promise)=>{
+  console.log(`Error: ${err.message}`.red.bold);
+
+  server.close( ()=>{
+    process.exit(1);
+  })
+})
