82986b3c9464804b93f5a966cdcdceb66e2c09aa
feelonewong <feelonewong@gmail.com>
2021-03-31 17:33:47 +0800

    处理选择和排序

--- controllers/mscamps.js
+++ controllers/mscamps.js
@@ -9,20 +9,35 @@ const asyncHandler = require("../middleware/async");
  */
 exports.getMscamps = asyncHandler(async (req, res, next) => {
   let query;
-  let reqQuery = {...req.query};
-  let removeFields = ["select"];
-  removeFields.forEach( (params)=> delete reqQuery[params] );
-
+  const reqQuery = { ...req.query };
+  // 处理关键字
+  const removeFields = ["select", "sort", "page", "limit"];
+  // 清除关键字及值
+  removeFields.forEach((param) => delete reqQuery[param]);
   let queryStr = JSON.stringify(reqQuery);
-  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match)=>`$${match}` ); 
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
   query = Mscamps.find(JSON.parse(queryStr));
-  if(req.query.select){
-    const fields = req.query.select.split(",").join();
+
+  // 在query所有数据的基础上,在加条件
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
     query = query.select(fields);
   }
-  console.log(query.select);
+
+  // 处理sort排序
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("createdAt");
+  }
 
   const mscamps = await query;
+
   res.status(200).json({
     success: true,
     data: mscamps,
@@ -67,50 +82,46 @@ exports.createMscamp = asyncHandler(async (req, res, next) => {
  * @route GET /api/v1/mscamps
  * @access 公开的
  */
-exports.updateMscamp = asyncHandler(
-  async (req, res, next) => {
-    try {
-      const mscamp = await Mscamps.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-      });
-      if (!mscamp) {
-        return next(
-          new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
-        );
-      }
-      res.status(200).json({
-        success: true,
-        data: mscamp,
-      });
-    } catch (error) {
-      next(error);
+exports.updateMscamp = asyncHandler(async (req, res, next) => {
+  try {
+    const mscamp = await Mscamps.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!mscamp) {
+      return next(
+        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+      );
     }
+    res.status(200).json({
+      success: true,
+      data: mscamp,
+    });
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 /**
  * @desc 删除单个的数据
  * @route GET /api/v1/mscamps
  * @access 公开的
  */
-exports.deleteMscamp = asyncHandler(
-  async (req, res, next) => {
-    try {
-      const mscamp = await Mscamps.findByIdAndDelete(req.params.id);
-      if (!mscamp) {
-        return next(
-          new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
-        );
-      }
-      res.status(200).json({
-        success: true,
-        data: {
-          message: "删除成功",
-        },
-      });
-    } catch (error) {
-      next(error);
+exports.deleteMscamp = asyncHandler(async (req, res, next) => {
+  try {
+    const mscamp = await Mscamps.findByIdAndDelete(req.params.id);
+    if (!mscamp) {
+      return next(
+        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+      );
     }
+    res.status(200).json({
+      success: true,
+      data: {
+        message: "删除成功",
+      },
+    });
+  } catch (error) {
+    next(error);
   }
-);
+});
