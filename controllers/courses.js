29f950d8e900888bebbd90816e290b226681eb70
feelonewong <feelonewong@gmail.com>
2021-04-01 17:17:52 +0800

    添加课程数据

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const Course = require("../models/Courses");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
+const Mscamp = require("../models/Mscamp");
 
 /**
  * @desc 获取所有的数据
@@ -24,3 +25,47 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     count: courses.length,
   });
 });
+
+/**
+ * @desc 根据ID获取单个数据
+ * @route GET /api/v1/courses/:id
+ * @access 公开的
+ */
+ exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: "mscamp",
+    select: "name description"
+  });
+  if (!course) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+
+/**
+ * @desc 创建单个课程数据
+ * @route GET /api/v1/courses/
+ * @access 公开的
+ */
+ exports.addCourse = asyncHandler(async (req, res, next) => {
+   //先查询mscamp的数据是否存在没查到才会添加
+  const mscamp = await Mscamp.findById(req.params.mscampId);
+  if (!mscamp) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  const course = await Course.create(req.body);
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
-const { getCourses } = require("../controllers/courses");
+const { getCourses, getCourse, addCourse } = require("../controllers/courses");
 
-router.route("/").get(getCourses);
+//直接获取请求
+router.route("/").get(getCourses).post(addCourse);
+
+//根据id获取请求
+router.route(`/:id`).get(getCourse);
+
 
 module.exports = router;
