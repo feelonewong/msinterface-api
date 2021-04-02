59f47e3a2c85d17eaf545a49507b946bb5ccc4ba
feelonewong <feelonewong@gmail.com>
2021-04-02 16:11:17 +0800

    创建token

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,8 +10,9 @@ const asyncHandler = require("../middleware/async");
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, role, email, password } = req.body;
   const user = await User.create({ name, role, email, password });
+  const token = user.getSingedJsonWebToken();
   res.status(200).json({
     success: true,
-    data: user,
+    data: token,
   });
 });
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { Schema } = mongoose;
 const UserSchema = Schema({
   name: {
@@ -38,4 +39,10 @@ UserSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+UserSchema.methods.getSingedJsonWebToken = function () {
+  const token = jwt.sign({ id: this._id, name: this.name }, process.env.PRIVATEKEY, {
+    expiresIn: process.env.EXPIRED,
+  });
+  return token;
+};
 module.exports = mongoose.model("User", UserSchema);
