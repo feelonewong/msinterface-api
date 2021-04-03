0403b6738e10421aa3700443970ec3b71920bca6
feelonewong <feelonewong@gmail.com>
2021-04-03 16:32:01 +0800

    路由守卫

--- controllers/mscamps.js
+++ controllers/mscamps.js
@@ -35,14 +35,12 @@ exports.getMscamp = asyncHandler(async (req, res, next) => {
  * @access 公开的
  */
 exports.createMscamp = asyncHandler(async (req, res, next) => {
-
   req.body.user = req.user.id;
 
-  const publistMscamp = await Mscamps.findOne({user: req.user.id});
+  const publistMscamp = await Mscamps.findOne({ user: req.user.id });
 
-
-  if(publistMscamp && req.user.role !== "admin"){
-    return next( new ErrorResponse("该机构已经存在不要重复创建",400) );
+  if (publistMscamp && req.user.role !== "admin") {
+    return next(new ErrorResponse("该机构已经存在不要重复创建", 400));
   }
 
   const mscamp = await Mscamps.create(req.body);
@@ -59,21 +57,27 @@ exports.createMscamp = asyncHandler(async (req, res, next) => {
  * @access 公开的
  */
 exports.updateMscamp = asyncHandler(async (req, res, next) => {
-  
-    const mscamp = await Mscamps.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!mscamp) {
-      return next(
-        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
-      );
-    }
-    res.status(200).json({
-      success: true,
-      data: mscamp,
-    });
-  
+  let mscamp = await Mscamps.findById(req.params.id);
+  if (!mscamp) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
+  }
+  //确定当前id和登录用户的id一致
+  if (mscamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(`该用户${req.params.id}无权限更新此数据`, 401)
+    );
+  }
+
+  mscamp = await Mscamps.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: mscamp,
+  });
 });
 
 /**
@@ -82,22 +86,25 @@ exports.updateMscamp = asyncHandler(async (req, res, next) => {
  * @access 公开的
  */
 exports.deleteMscamp = asyncHandler(async (req, res, next) => {
-  try {
-    const mscamp = await Mscamps.findById(req.params.id);
-    if (!mscamp) {
-      return next(
-        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    mscamp.remove();
-    res.status(200).json({
-      success: true,
-      data: {
-        message: "删除成功",
-      },
-    });
-  } catch (error) {
-    next(error);
+  const mscamp = await Mscamps.findById(req.params.id);
+  if (!mscamp) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
   }
+
+  //确定当前id和登录用户的id一致
+  if (mscamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(`该用户${req.params.id}无权限删除此数据`, 401)
+    );
+  }
+
+  mscamp.remove();
+  res.status(200).json({
+    success: true,
+    data: {
+      message: "删除成功",
+    },
+  });
 });
